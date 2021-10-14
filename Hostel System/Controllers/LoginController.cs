a0b6e49de23bf1d99857f023778fcc3303894272
poglodek <pawelogll@gmail.com>
2021-10-14 15:39:27 +0200

    Adding login view

--- Hostel System/Controllers/LoginController.cs
+++ Hostel System/Controllers/LoginController.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hostel_System.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hostel_System.Controllers
 {
     public class LoginController : Controller 
     {
+        [HttpGet]
         public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(LoginModel loginModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.ErrorMessage = "Email and Password is required";
+                return View();
+            }
+
+            return Ok("ok");
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterModel registerModel)
         {
             return View();
         }
